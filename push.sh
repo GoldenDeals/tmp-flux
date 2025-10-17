fc20e93ef40b1111755c32f24086b13ce03bfb98
Иван Неботов <i.nebotov@f6.ru>
2025-10-17 16:02:00 +0300

    experiment

--- push.sh
+++ push.sh
@@ -7,13 +7,13 @@ TAG=$(git rev-parse HEAD)
 # Flux system
 flux push artifact oci://ghcr.io/goldendeals/flux-hq:$TAG \
   --path="clusters/hq" \
-  --source="$(git config --get remote.github.url)" \
+  --source="$(git config --get remote.origin.url)" \
   --revision="$TAG" 
 flux tag artifact oci://ghcr.io/goldendeals/flux-hq:$TAG --tag deploy
 
 # Base
 flux push artifact oci://ghcr.io/goldendeals/base:$TAG \
   --path="base" \
-  --source="$(git config --get remote.github.url)" \
+  --source="$(git config --get remote.origin.url)" \
   --revision="$TAG" 
 flux tag artifact oci://ghcr.io/goldendeals/base:$TAG --tag deploy
